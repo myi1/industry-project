7fc7aeb1c72fde92abb7158eb69f4a234ecdf021
Yoseph Wondwossen <yoseph.wondwossen@gmail.com>
2021-06-11 14:54:41 -0400

    worked on forms

--- client/src/component/DoctorInfo/DoctorInfo.js
+++ client/src/component/DoctorInfo/DoctorInfo.js
@@ -1,26 +1,35 @@
 import React from "react";
+import "./DoctorInfo.scss"
 
 function DoctorInfo() {
   return (
     <div className='doctorInfo'>
-      <div>
-        <img></img>
-        <p>
-          is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley
-        </p>
-      </div>
-      <div>
-        <button>Book Consultation</button>
-      </div>
-      <div className='doctorInfo__previous-container'>
-        <h3 className='doctorInfo__previous-header'>Previous Visits</h3>
-        <ul className='doctorInfo__previous-visits-list'>
-          <li className='doctorInfo__previous-visit'>September 24th, 2020</li>
-          <li className='doctorInfo__previous-visit'>July 18th, 2020</li>
-          <li className='doctorInfo__previous-visit'>February 2nd, 2020</li>
-        </ul>
+      <div className='doctorInfo__details-section'>
+        <div className="doctorInfo__info-card">
+        <img
+              src='http://placekitten.com/g/100/100'
+              alt='Doctor'
+              className='doctorInfo__doctor-img'
+            />
+          <div className="doctorInfo__details">
+            <p className="doctorInfo__info doctorInfo__weight"> Dr. Chase Gill</p>
+            <p className="doctorInfo__info"> Miami, Florida</p>
+            <p className="doctorInfo__info"> Miami Neuroscience Center</p>
+            <p className="doctorInfo__info">+1 786 871 6800</p>
+            <div>
+              <button className="doctorInfo__btn">Book an Appointment</button>
+            </div>
+          </div>
+        </div>
+      
+        <div className='doctorInfo__previous-container'>
+          <h3 className='doctorInfo__previous-header'>Previous Visits</h3>
+          <ul className='doctorInfo__previous-visits-list'>
+            <li className='doctorInfo__previous-visit'>September 24th, 2020</li>
+            <li className='doctorInfo__previous-visit'>July 18th, 2020</li>
+            <li className='doctorInfo__previous-visit'>February 2nd, 2020</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- client/src/component/Form/Form.js
+++ client/src/component/Form/Form.js
@@ -1,65 +1,49 @@
 import React from "react";
+import "./Form.scss";
 
 function Form() {
   return (
     <form className='form'>
-      <h2 className='form__header'>Header</h2>
-      <input
-        type='text'
-        name='location'
-        className='form__location'
-        placeholder='Where does it hurt'
-      />
+      <h2 className='form__label'>Appointment Details</h2>
+      <input className='form__location' list='location' />
+      <datalist name='location' id='location' className='form__location'>
+        <option value='Chronic back pain' />
+        <option value='Chest pain' />
+        <option value='Knee pain' />
+        <option value='Joint pain' />
+        <option value='Chronic headaches' />
+        <option value='Arthritis' />
+      </datalist>
       <label className='form__label'>
         Description of issue
         <input type='text' name='description' className='form__description' />
       </label>
 
-      <button className='form__cta'>CTA for Avatar</button>
+      <button className='form__cta'>File Uploaded</button>
       <div class='form__select-date-container'>
-        <h3 className='form__select-date-header'>Availability</h3>
+        <h3 className='form__label'>Availability</h3>
         <div className='form__container'>
           <div className='form__date-container'>
-            <p className='form__date'>11-06-2021</p>
+            <p className='form__date'>June 11</p>
             <ul className='form__times'>
               <li>10:00 AM</li>
-              <li>10:15 AM</li>
-              <li>11:30 AM</li>
-              <li>12:45 PM</li>
-              <li>2:45 PM</li>
-              <li>4:45 PM</li>
             </ul>
             <div className='form__date-container'>
-              <p className='form__date'>14-06-2021</p>
+              <p className='form__date'>June 12</p>
               <ul className='form__times'>
                 <li>10:00 AM</li>
-                <li>10:15 AM</li>
-                <li>11:30 AM</li>
-                <li>12:45 PM</li>
-                <li>2:45 PM</li>
-                <li>4:45 PM</li>
               </ul>
             </div>
             <div className='form__date-container'>
-              <p className='form__date'>15-06-2021</p>
+              <p className='form__date'>June 13</p>
               <ul className='form__times'>
                 <li>10:00 AM</li>
-                <li>10:15 AM</li>
-                <li>11:30 AM</li>
-                <li>12:45 PM</li>
-                <li>2:45 PM</li>
-                <li>4:45 PM</li>
               </ul>
             </div>
             <div className='form__date-container'>
-              <p className='form__date'>16-06-2021</p>
+              <p className='form__date'>June 14</p>
               <ul className='form__times'>
                 <li>10:00 AM</li>
-                <li>10:15 AM</li>
-                <li>11:30 AM</li>
-                <li>12:45 PM</li>
-                <li>2:45 PM</li>
-                <li>4:45 PM</li>
               </ul>
             </div>
           </div>
